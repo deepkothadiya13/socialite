d3a588dda4f1b222b17ddbafde2c9133b5ec195b
deepkothadiya13 <51197199+deepkothadiya13@users.noreply.github.com>
2019-06-04 19:19:15 +0530

    Login page

--- mysite/webapp/views.py
+++ mysite/webapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.template import loader
 
 # Create your views here.
+def home(request):
+    # template = loader.get_template('webapp/login.html')
+    # return HttpResponse(template)
+    return render(request,'webapp/login.html')
--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import requests
 print("hello")
-link = "https://www.makemytrip.com"
+link = "https://www.linkedin.com/feed/"
 html = requests.get(link, params=None)
 print(html.text)
 print(type(html))
-
-# https://www.makemytrip.com/flight/search?itinerary=DEL-BLR-01/06/2019&tripType=O&paxType=A-1_C-0_I-0&intl=false&=&cabinClass=E
